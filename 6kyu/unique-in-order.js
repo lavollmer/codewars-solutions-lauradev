f18d6cbd2b2343e15e86a3514434f1c10d7fda88
Laura <laura.e.vollmer@gmail.com>
2024-11-20 14:07:34 -0600

    working on a solution

---
+++ 6kyu/unique-in-order.js
@@ -0,0 +1,17 @@
+var uniqueInOrder=function(iterable){
+
+    if (iterable.length === 0){
+      return [];
+    }
+    
+    let result = [iterable[0]];
+    
+    for (var i = 0; i < iterable.length; i++){
+      
+      if (iterable[i] !== iterable[i - 1]){
+        result.push(iterable[i]);
+      }
+    }
+  
+    return result;
+  }
